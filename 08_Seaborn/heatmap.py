62a6b3e8ad7d5665acda391162f49d00400f3a93
Kira Schuman <kira.schuman@gmail.com>
2017-12-08 23:37:21 -0500

    Update

--- 08_Seaborn/heatmap.py
+++ 08_Seaborn/heatmap.py
@@ -1,7 +1,7 @@
 # Libraries
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 # Read data
 SA = pd.read_csv('south_amer.csv', encoding = "ISO-8859-1")
@@ -12,15 +12,25 @@ grp = grp.drop(0)
 
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
           'August', 'September', 'October', 'November', 'December']
+z = grp.values.tolist()
+z.reverse()
+years = list(range(1970, 2017))
 
 
-# Plot
-y = sns.heatmap(grp, yticklabels=months, cmap="YlGnBu")
-plt.yticks(rotation = 0, size='x-small')
-plt.xticks(rotation = 45, size='x-small')
-plt.xlabel('Year')
-plt.ylabel('Month')
-plt.tight_layout()
-plt.title('Yearly Trends in Attacks')
-#plt.show()
-plt.savefig('08_Seaborn.jpg')
+data = [
+    go.Heatmap(
+        z=z,
+        x=years,
+        y=months,
+        colorscale='Viridis',
+    )
+]
+
+layout = go.Layout(
+    title='Attacks by Month',
+    xaxis = dict(ticks=''),
+    yaxis = dict(ticks='' )
+)
+
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='08_heatmap')
